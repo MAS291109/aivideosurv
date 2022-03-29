0d8b3d7cfe166ac0ab2171917da0d11f583c256f
MAS291109 <81005913+MAS291109@users.noreply.github.com>
2022-03-29 17:30:02 +0530

    Add files via upload
    added video, model loaded.

---
+++ main.js
@@ -0,0 +1,28 @@
+video = "";
+
+function preload(){
+    video = createVideo("video.mp4");
+    video.hide();
+}
+
+function setup(){
+    canvas = createCanvas(450, 350);
+    canvas.center();
+}
+
+function draw(){
+    image(video, 0, 0, 450, 350);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+    video.speed(1);
+    video.volume(0);
+    video.loop();
+}
