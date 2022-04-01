65a524193f9f968af299caf2dc2e4309191d7046
MAS291109 <81005913+MAS291109@users.noreply.github.com>
2022-04-01 17:44:28 +0530

    Add files via upload
    gotResult, random fill and stroke color, completed draw function.

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 video = "";
+objects = [];
 
 function preload(){
     video = createVideo("video.mp4");
@@ -12,6 +13,25 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 450, 350);
+    if(status != "")
+    {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+
+        objectDetector.detect(video, gotResult);
+        for(i = 0; i < objects.length; i++){
+            document.getElementById("status").innerHTML = "Status : Objects Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are : "+objects.length;
+            
+            fill(r,g,b);
+            percent = floor(objects[i].confidence *100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y - 5);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
 
 function start(){
@@ -25,4 +45,12 @@ function modelLoaded() {
     video.speed(1);
     video.volume(0);
     video.loop();
+}
+
+function gotResult(error, results){
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
